1a8166e2a507b030d9a9c01261adae7d1fc8194f
ShevchenkoYuliia <yuliia.shevchenko1845@gmail.com>
2024-03-24 23:44:10 +0200

    Create Author.java

---
+++ Lab2/Variant1/Author.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class Author extends Human implements Serializable  {
+    public Author(String name, String surname){
+        super(name, surname);
+    }
+    public String getName() {
+        return super.getName();
+    }
+    public void setName(String name) {
+        super.setName(name);
+    }
+    public String getSurname() {
+        return super.getSurname();
+    }
+
+    public void setSurname(String surname) {
+        super.setSurname(surname);
+    }
+    public String toString(){
+        return "name: " + getName() + ", surname: " + getSurname();
+    }
+}
